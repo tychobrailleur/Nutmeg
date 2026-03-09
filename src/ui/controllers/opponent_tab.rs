6e9a64e92f7dd1b66495f5b328a6edb2be5bc3f5
Sébastien Le Callonnec <sebastien@weblogism.com>
2026-03-09 08:50:45 +0000

    Don't do background calls to CHPP.

--- src/ui/controllers/opponent_tab.rs
+++ src/ui/controllers/opponent_tab.rs
@@ -1,14 +1,9 @@
 use crate::chpp::client::HattrickClient;
-use crate::config::{consumer_key, consumer_secret};
 use crate::opponent_analysis::ui::model;
 use crate::service::opponent_analysis::OpponentAnalysisService;
-use crate::service::secret::SystemSecretService;
 use crate::ui::context_object::ContextObject;
 use crate::ui::team_object::TeamObject;
 use gtk::gio;
-use gtk::glib;
-use gtk::prelude::*;
-use log::debug;
 use std::sync::Arc;
 
 pub struct OpponentTabController {
@@ -50,49 +45,6 @@ impl OpponentTabController {
                 list_store.append(&item);
             }
         }
-
-        // 2. Refresh from API in background if secrets available
-        glib::MainContext::default().spawn_local(async move {
-            let secret_service = SystemSecretService::new();
-            use crate::service::secret::SecretStorageService;
-
-            let token_res = secret_service.get_secret("access_token").await;
-            let secret_res = secret_service.get_secret("access_secret").await;
-
-            if let (Ok(Some(token)), Ok(Some(secret))) = (token_res, secret_res) {
-                let ck = consumer_key();
-                let cs = consumer_secret();
-
-                let get_auth =
-                    || crate::chpp::oauth::create_oauth_context(&ck, &cs, &token, &secret);
-
-                match service.get_upcoming_opponents(&get_auth, our_team_id).await {
-                    Ok(opponents) => {
-                        list_store.remove_all();
-                        for opp in opponents {
-                            let logo_url = format!(
-                                "//res.hattrick.org/teamlogo/{}/{}/{}/{}/{}.png",
-                                opp.team_id % 10,
-                                opp.team_id % 100,
-                                opp.team_id % 1000,
-                                opp.team_id,
-                                opp.team_id
-                            );
-                            let item = model::OpponentItem::new(
-                                opp.team_id,
-                                &opp.team_name,
-                                &opp.match_date,
-                                &logo_url,
-                            );
-                            list_store.append(&item);
-                        }
-                    }
-                    Err(_e) => {
-                        debug!("Failed to refresh upcoming opponents from API: {}", _e);
-                    }
-                }
-            }
-        });
     }
 
     pub fn clear(&self) {
